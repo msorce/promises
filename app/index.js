dc728f56f65799e27c87a4de211d2f1a4662f4eb
msorce <marksorce@gmail.com>
2016-12-12 16:40:39 -0800

    implemented promise service and injected into mainCtrl

--- app/index.js
+++ app/index.js
@@ -1,12 +1,19 @@
 angular = require('angular');
 
 var app = angular.module('app', []);
-app.service('testService', function(){
-  this.test = 'testing';
-});
+app.service('testService', function($q, $timeout) {
 
-app.controller('mainCtrl', function($scope, testService){
-  $scope.fn = function(){
-    console.log('from main controller', testService.test);
-  };
+  this.getStuff = $q(function(resolve, reject) {
+    console.log('getting stuff');
+    $timeout(function() {
+      resolve('got stuff');
+    }, 2000);
+  });
+}); //end getStuff
+
+
+app.controller('mainCtrl', function($scope, testService) {
+  testService.getStuff.then(function(data) {
+    console.log(data);
+  });
 });
