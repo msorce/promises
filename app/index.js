b269a24b56714dc341ffdc7cb15f1e7893404849
msorce <marksorce@gmail.com>
2016-12-15 11:10:09 -0800

    implemented ES6 new Promise

--- app/index.js
+++ app/index.js
@@ -5,14 +5,29 @@ app.service('testService', testService);
 
 function testService($q, $timeout) {
   this.getStuff = () => {
+
     return $q((resolve, reject) => {
       var log = document.getElementById('log');
-      log.insertAdjacentHTML('beforeend', `<div>getting Stuff</div>`);
+      log.insertAdjacentHTML('beforeend', `<div>getting Stuff $q</div>`);
+
       $timeout(() => {
-        resolve('got stuff');
+        resolve('got with $q');
       }, 2000);
+
     });
   };
+
+  this.getWithES6 = () => {
+    return new Promise(function(resolve, reject){
+
+      var log = document.getElementById('log');
+      log.insertAdjacentHTML('beforeend', `<div>getting Stuff ES6</div>`);
+
+      $timeout(() => {
+        resolve('got with ES6')
+      }, 2000)
+    })
+  }
 }
 
 app.controller('mainCtrl', mainCtrl);
@@ -20,6 +35,13 @@ app.controller('mainCtrl', mainCtrl);
 function mainCtrl($scope, testService) {
   $scope.fn = () => {
     var promise = testService.getStuff();
+    promise.then((resolvedValue) => {
+      log.insertAdjacentHTML('beforeend', `<div> ${resolvedValue} </div>`);
+    });
+  };
+
+  $scope.fn2 = () => {
+    var promise = testService.getWithES6();
     promise.then((resolvedValue) => {
       log.insertAdjacentHTML('beforeend', `<div> ${resolvedValue} </div>`);
     });
